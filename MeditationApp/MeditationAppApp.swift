81229f3effcfeef16ad4d958ded1602ee6ff580d
Raunak Priya <raunakpriya@gmail.com>
2023-03-07 20:21:04 +0530

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Harsh Raj on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
